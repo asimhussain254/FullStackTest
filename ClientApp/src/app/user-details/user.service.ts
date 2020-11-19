4a513f65a88889075dbafe673b1425156c5b137f
Asad Sahi <asadazmat@gmail.com>
2020-11-19 12:30:57 +0000

    user service

--- ClientApp/src/app/user-details/user.service.ts
+++ ClientApp/src/app/user-details/user.service.ts
@@ -1,26 +1,33 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
-import { IUser } from './user.model';
+import { IUser } from "./user.model";
 
-@Injectable()
+@Injectable({
+  providedIn: "root",
+})
 export class UserService {
   selectedUser: IUser;
   userList: IUser[];
   constructor(private httpClient: HttpClient) {}
 
   getUsers() {
-    this.httpClient.get('https://localhost:5001/api/User').subscribe((res) => (this.userList = res as IUser[]));
+    this.httpClient
+      .get("https://localhost:5001/api/User")
+      .subscribe((res) => (this.userList = res as IUser[]));
   }
 
   createUser(user: IUser) {
-    return this.httpClient.post('https://localhost:5001/api/User', user);
+    return this.httpClient.post("https://localhost:5001/api/User", user);
   }
 
   updateUser(userId: number, user: IUser) {
-    return this.httpClient.put(`https://localhost:5001/api/User/${userId}`, user);
+    return this.httpClient.put(
+      `https://localhost:5001/api/User/${userId}`,
+      user
+    );
   }
-  deleteUser(userId: number){
+  deleteUser(userId: number) {
     return this.httpClient.delete(`https://localhost:5001/api/User/${userId}`);
   }
 }
